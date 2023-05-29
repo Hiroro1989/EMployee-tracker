07d5e8070fa3055d41b578b57c0f924996bcb662
hiroro <hiroaki.a1989@gmail.com>
2023-05-29 16:54:59 -0400

    refactoring index.js file

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 const mysql = require("mysql2");
 const inquire = require("inquirer");
 const cTable = require("console.table");
-// const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
-// inquirer.registerPrompt("maxlength-input", MaxLengthInputPrompt);
 
-//connecteing database
+//connecting DB
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -16,40 +14,8 @@ const db = mysql.createConnection(
   console.log(`conndected to the employees_db`)
 );
 
-const sqlShowing = ({ query }) => {
-  let queryText;
-  switch (query) {
-    case "View All Departments":
-      queryText = `SELECT * FROM department`;
-      break;
-
-    case "View All Roles":
-      queryText = `SELECT role.id, role.title, department.name, role.salary FROM role INNER JOIN department ON department.id = role.department_id`;
-      break;
-
-    case "View All Employees":
-      queryText = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON m.id = employee.manager_id;`;
-      break;
-    case "Add a Department":
-        queryText = `addD`;
-        break;
-    case "Add a Role":
-        queryText =`addR`;
-        break;
-    case "Add an Employee":
-        queryText =`addE`;
-        break;
-    case "Quit":
-      console.log("Exiting the program....");
-      process.exit();
-      break;
-    default:
-      queryText = "";
-  }
-  return queryText;
-};
-
-function promptUser() {
+//initial function
+const init = () => {
   inquire
     .prompt([
       {
@@ -67,133 +33,130 @@ function promptUser() {
         ],
       },
     ])
-    .then((res) => {
-      const content = sqlShowing(res);
+    .then((answers) => {
+      switch (answers.query) {
+        case "View All Departments":
+          showSqlDP();
+          break;
 
-      if (
-        content === "SELECT * FROM department" ||
-        content ===
-          "SELECT role.id, role.title, department.name, role.salary FROM role INNER JOIN department ON department.id = role.department_id" ||
-        content ===
-          'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON m.id = employee.manager_id;'
-      ) {
-        db.query(content, (err, result) => {
-          if (result) {
-            console.log("");
-            console.table(result);
-            promptUser(); // Recursive call to ask the question again
-          } else {
-            console.error(err);
-          }
-        });
-      } else if (content == "addD") {
-        const newDepartment = ()=>
-        inquire.prompt({
-            name: "department",
-            type: "input",
-            message: "What is the name of department?"
-        })
-        .then((res)=>{
-            const param = newDepartment(res);
-            db.query(`INSERT INTO department (name) VALUES (?)`, [param.department] ,(err,res) =>{
-                if (res){
-                    console.log('Succesfully Added!');
-                    promptUser();
-                }else{
-                    console.error(err);
-                    promptUser();
-                }
-            } )
-        })
-        console.log("addD");
-        promptUser(); // Recursive call to ask the question again
-      } else if (content == "addR"){
-        console.log("addR");
-        promptUser(); // Recursive call to ask the question again
-      }else if(content == "addE"){
-        console.log("addE");
-        promptUser();
-      }else{
-        console.log('Other logic');
-        promptUser(); 
+        case "View All Roles":
+          showSqlRL();
+          break;
+
+        case "View All Employees":
+          showSqlEM();
+          break;
+
+        case "Add a Department":
+            addSqlDP();
+            break;
+
+        case "Add a Role":
+            addSqlRL();
+            break;
+
+        case "Quit":
+          console.log("Exiting the program....");
+          process.exit();
+          break;
       }
+    })
+    .catch((error) => {
+      console.log("An error occurred:", error);
     });
+};
+
+//function for showing department table
+const showSqlDP = () => {
+  db.query(`SELECT * FROM department`, (err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.table(res);
+    }
+    init();
+  });
+};
+
+const showSqlRL = () => {
+  db.query(
+    `SELECT role.id, role.title, department.name, role.salary FROM role INNER JOIN department ON department.id = role.department_id`,
+    (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.table(res);
+      }
+      init();
+    }
+  );
+};
+
+const showSqlEM = () => {
+  db.query(
+    `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON m.id = employee.manager_id;`,
+    (err, res) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.table(res);
+      }
+      init();
+    }
+  );
+};
+
+const addSqlDP = () => {
+    inquire.prompt([
+        {
+            type: "input",
+            name: "newDepartment",
+            message: "What is the name of department?"
+        }
+    ])
+    .then((data)=>{
+        const sqlQuery = `INSERT INTO department (name) VALUE ("${data.newDepartment}")`;
+        // const params = [data.newDepatment];
+        db.query(sqlQuery, (err, result) =>{
+            if (result){
+                console.log("Successfully added!");
+            }else{
+                console.error(err);
+            }
+            init();
+        })
+    })
+};
+
+const addSqlRL = () => {
+    inquire.prompt([
+        {
+            type: "input",
+            name: "newRole",
+            message: "What is the name of the role?"
+        },
+        {
+            type: "input",
+            name: "newSalary",
+            message: "What is the salary of the role?"
+        },
+        {
+            type: "list",
+            name: "newRoleBelong",
+            message: "Which department does the role belong to?",
+            // choices: need to add 
+        }
+    ]).then((data)=>{
+        const sqlQuery = `INSERT INTO role (title, salary, department_id) VALUE ("${data.newRole}, ${data.newSalary},${list[data.newRoleBelong]}")`;
+        db.query(sqlQuery, (err, result) =>{
+            if (result){
+                console.log("Successfully added!");
+            }else{
+                console.error(err);
+            }
+            init();
+        })
+    })
 }
 
-promptUser(); // Initial call to start the prompt
-
-//     inquire.prompt(
-//         [
-//             {
-//                 type: "list",
-//                 name: "query",
-//                 message: "What would you like to do?",
-//                 choices: [
-//                    "View All Departments",
-//                    "View All Roles",
-//                    "View All Employees",
-//                    "Quit",
-//                 ],
-//             },
-//         ],
-//      )
-// .then((res) =>{
-//     const content = sqlShowing(res);
-
-//     if(content === 'SELECT * FROM department' || 'SELECT role.id, role.title, department.name, role.salary FROM role INNER JOIN department ON department.id = role.department_id' || 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON m.id = employee.manager_id;'){
-//         db.query(content, (err, result) =>{
-//             if (result){
-//                 console.log('')
-//                 console.table(result);
-//                 promptUser();
-//             } else {
-//                 console.error(err)
-//             }
-//         });
-//     }else {console.log('done')}
-
-// db.query(content, (err, result) =>{
-//     if (result){
-//         console.log('')
-//         console.table(result);
-//         return inquire;
-//     } else {
-//         console.error(err)
-//     }
-// });
-//  })
-
-//reading eachtables
-//showing depeartment table
-// const sqlShowDP =`SELECT * FROM department`
-
-// db.query(sqlShowDP, (err, result) =>{
-//     if (result){
-//         console.log('')
-//         console.table(result);
-//     } else {
-//         console.error(err)
-//     }
-// });
-// //showing role table
-// const sqlShowRole = `SELECT * FROM role`
-
-// db.query(sqlShowRole, (err, result) =>{
-//     if (result){
-//         console.log('')
-//         console.table(result);
-//     } else {
-//         console.error(err)
-//     }
-// });
-
-// //showing employee table
-// const sqlShowEmployee = `SELECT * FROM employee`
-// db.query(sqlShowEmployee, (err, result) =>{
-//     if (result){
-//         console.log('')
-//         console.table(result);
-//     } else {
-//         console.error(err)
-//     }
-// })
+init();
---
+++ indexBack.js
@@ -0,0 +1,215 @@
+//install packages
+const mysql = require("mysql2");
+const inquire = require("inquirer");
+const cTable = require("console.table");
+// const Connection = require("mysql2/typings/mysql/lib/Connection");
+// const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
+// inquirer.registerPrompt("maxlength-input", MaxLengthInputPrompt);
+
+//connecteing database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "hiro1108",
+    database: "employees_db",
+  },
+  console.log(`conndected to the employees_db`)
+);
+
+const sqlShowing = ({ query }) => {
+  let queryText;
+  switch (query) {
+    case "View All Departments":
+      //   queryText = `SELECT * FROM department`;
+      showSqlDP();
+      break;
+
+    case "View All Roles":
+      //   queryText = `SELECT role.id, role.title, department.name, role.salary FROM role INNER JOIN department ON department.id = role.department_id`;
+      break;
+
+    case "View All Employees":
+      queryText = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON m.id = employee.manager_id;`;
+      break;
+    case "Add a Department":
+      queryText = `addD`;
+      break;
+    case "Add a Role":
+      queryText = `addR`;
+      break;
+    case "Add an Employee":
+      queryText = `addE`;
+      break;
+    case "Quit":
+      console.log("Exiting the program....");
+      process.exit();
+      break;
+    default:
+      queryText = "";
+  }
+  return queryText;
+};
+
+function promptUser() {
+  inquire.prompt([
+    {
+      type: "list",
+      name: "query",
+      message: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Quit",
+      ],
+    },
+  ]);
+
+  
+  //     .then((res) => {
+  //       const content = sqlShowing(res);
+
+  //       if (
+  //         content === "SELECT * FROM department" ||
+  //         content ===
+  //           "SELECT role.id, role.title, department.name, role.salary FROM role INNER JOIN department ON department.id = role.department_id" ||
+  //         content ===
+  //           'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON m.id = employee.manager_id;'
+  //       ) {
+  //         db.query(content, (err, result) => {
+  //           if (result) {
+  //             console.log("");
+  //             console.table(result);
+  //             promptUser(); // Recursive call to ask the question again
+  //           } else {
+  //             console.error(err);
+  //           }
+  //         });
+  //       } else if (content == "addD") {
+  //         const newDepartment = () =>
+  //         inquire.prompt({
+  //             name: "department",
+  //             type: "input",
+  //             message: "What is the name of department?"
+  //         })
+  //         .then((res)=>{
+  //             const param = newDepartment(res);
+  //             db.query(`INSERT INTO department (name) VALUES (?)`, [param.department] ,(err,res) =>{
+  //                 if (res){
+  //                     console.log('Succesfully Added!');
+  //                     promptUser();
+  //                 }else{
+  //                     console.error(err);
+  //                     promptUser();
+  //                 }
+  //             } )}
+  //         )
+  //         console.log("addD");
+  //         promptUser(); // Recursive call to ask the question again
+  //       } else if (content == "addR"){
+  //         console.log("addR");
+  //         promptUser(); // Recursive call to ask the question again
+  //       }else if(content == "addE"){
+  //         console.log("addE");
+  //         promptUser();
+  //       }else{
+  //         console.log('Other logic');
+  //         promptUser();
+  //       }
+  //     });
+}
+
+const showSqlDP = () => {
+    db.query(`SELECT * FROM department`, (err, res) => {
+      if (err) {
+        console.error(err);
+        promptUser();
+      } else {
+        console.table(res);
+        promptUser();
+      }
+    });
+  };
+
+promptUser();
+// Initial call to start the prompt
+
+//     inquire.prompt(
+//         [
+//             {
+//                 type: "list",
+//                 name: "query",
+//                 message: "What would you like to do?",
+//                 choices: [
+//                    "View All Departments",
+//                    "View All Roles",
+//                    "View All Employees",
+//                    "Quit",
+//                 ],
+//             },
+//         ],
+//      )
+// .then((res) =>{
+//     const content = sqlShowing(res);
+
+//     if(content === 'SELECT * FROM department' || 'SELECT role.id, role.title, department.name, role.salary FROM role INNER JOIN department ON department.id = role.department_id' || 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(m.first_name," ",m.last_name) AS manager FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON department.id = role.department_id LEFT JOIN employee m ON m.id = employee.manager_id;'){
+//         db.query(content, (err, result) =>{
+//             if (result){
+//                 console.log('')
+//                 console.table(result);
+//                 promptUser();
+//             } else {
+//                 console.error(err)
+//             }
+//         });
+//     }else {console.log('done')}
+
+// db.query(content, (err, result) =>{
+//     if (result){
+//         console.log('')
+//         console.table(result);
+//         return inquire;
+//     } else {
+//         console.error(err)
+//     }
+// });
+//  })
+
+//reading eachtables
+//showing depeartment table
+// const sqlShowDP =`SELECT * FROM department`
+
+// db.query(sqlShowDP, (err, result) =>{
+//     if (result){
+//         console.log('')
+//         console.table(result);
+//     } else {
+//         console.error(err)
+//     }
+// });
+// //showing role table
+// const sqlShowRole = `SELECT * FROM role`
+
+// db.query(sqlShowRole, (err, result) =>{
+//     if (result){
+//         console.log('')
+//         console.table(result);
+//     } else {
+//         console.error(err)
+//     }
+// });
+
+// //showing employee table
+// const sqlShowEmployee = `SELECT * FROM employee`
+// db.query(sqlShowEmployee, (err, result) =>{
+//     if (result){
+//         console.log('')
+//         console.table(result);
+//     } else {
+//         console.error(err)
+//     }
+// })
